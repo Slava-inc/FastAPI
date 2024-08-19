594b5b24371c9b08e9a67b6d3eafaddbffa97a35
Slava-inc <v.danchenko@voshod.local>
2024-08-19 09:55:40 +0300

    start server - FastAPI, Swagger UI

--- app/users/database.py
+++ app/users/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./db.sqlite"
 
-engine = create_engine(DATABASE_URL, connect_args={'check_same_thred': False})
+engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
