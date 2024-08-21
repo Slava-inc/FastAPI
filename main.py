3e0880e26394911017c30c5b90ff9d3658f5e989
Slava-inc <v.danchenko@voshod.local>
2024-08-21 11:08:53 +0300

    file uploading

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from app.map_object.schemas import MapCreate
 from app.map_object.crude import submitData
 
 import json
+from fastapi import File, UploadFile
+
 # FastAPI instance creation
 app = FastAPI()
 
@@ -26,8 +28,15 @@ async def register(username: str, email: str, password: str):
 @app.post("/submitData/")
 async def submitData(data):
     kwargs = json.loads(data)
-    map_create = MapCreate(title=kwargs['title'], other_titles=kwargs['other_titles'], user=kwargs['user'])
+    map_create = MapCreate(title=kwargs['title'], other_titles=kwargs['other_titles'], 
+                           connect=kwargs['connect'], add_time=kwargs['add_time'], user=kwargs['user'],
+                           coords=str(kwargs['coords']), level=str(kwargs['level']), images=kwargs['images'])
     map = submitData(db=SessionLocal(), map_create=map_create)
     if map == None:
         return 'during submition error raised'
     return f'user {map.title} added!'
+
+@app.post("/upload/")
+async def create_upload_file(file: UploadFile):
+    # do something with the file
+    return {"filename": file.filename}
